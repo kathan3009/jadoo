ca198348a346d46b53a2b08c1c01ea0104266979
kathan3009 <kthndesai@gmail.com>
2019-08-09 02:14:54 +0530

    lol

---
+++ app.js
@@ -0,0 +1,14 @@
+/*
+const modeBtn = document
+  .querySelector("#dark")
+  .addEventListener("click",function(){
+      const everything=document.querySelectorAll('*')
+      const projectFiles=document.querySelectorAll('.image');
+      everything.forEach((item) => {
+          item.classList.toggle('dark');
+      });
+      projectFiles.forEach((item)=> {
+          item.classList.remove('dark');
+      })
+  });
+  */
